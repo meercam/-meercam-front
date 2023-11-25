09b2b2eee05f5811a70a1fa702428d589b2b6e50
Cuzz <rygus9@naver.com>
2023-11-26 07:06:38 +0900

    feat: 디자인

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,42 @@
-import { useState, useEffect } from 'react';
-import io from 'socket.io-client';
-
-const socket = io('http://localhost:5000'); // Flask 서버의 주소
-
 const App = () => {
-  const [message, setMessage] = useState('');
-  const [chat, setChat] = useState<string[]>([]);
-
-  useEffect(() => {
-    socket.on('message', (msg: string) => {
-      setChat(prevChat => [...prevChat, msg]);
-    });
-
-    return () => {
-      socket.off('message');
-    };
-  }, []);
-
-  const sendMessage = () => {
-    socket.emit('message', message);
-    setMessage('');
-  };
-
   return (
-    <div>
-      <h1>Simple Chat</h1>
-      <input 
-        type="text" 
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        onKeyPress={(e) => e.key === 'Enter' ? sendMessage() : null}
-      />
-      <button onClick={sendMessage}>Send</button>
-      <div>
-        {chat.map((msg, index) => (
-          <p key={index}>{msg}</p>
-        ))}
-      </div>
+    <div className="bg-gray-100 min-h-screen h-full">
+      <header className="bg-white mb-6">
+        <div className="max-w-6xl m-auto w-full flex items-center gap-4 py-2 px-4">
+          <img
+            src="/Meerkat_logo.png"
+            alt="미어캣 로고"
+            className="w-16 h-16 object-cover"
+          />
+          <h2 className="text-3xl font-semibold">Meerkat</h2>
+        </div>
+      </header>
+      <main className="max-w-6xl m-auto w-full flex gap-6 px-4">
+        <div className="flex-1 flex gap-6 flex-col">
+          <div className="w-full rounded-md bg-white p-6">
+            <h3 className="text-xl pb-2 font-semibold">실시간 영상</h3>
+            <div className="w-full h-[380px] bg-gray-400 rounded-md"></div>
+          </div>
+          <div className="bg-white rounded-md p-6">
+            <h3 className="text-xl pb-2 font-semibold">CCTV 리스트</h3>
+            <div className="w-full flex items-start gap-4">
+              {['공원1', '공원2', '행사장1'].map((elem) => (
+                <button className="w-36 h-10 flex items-center justify-center rounded-md border border-gray-300">
+                  <span className="text-lg">{elem}</span>
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="w-80 bg-white rounded-md p-6">
+          <h3 className="text-xl pb-4 font-semibold">Alert Log</h3>
+          <div className="">
+            
+          </div>
+        </div>
+      </main>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
