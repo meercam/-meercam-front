82c7446348cd10f3bfbe58029ccdd10c56c8a652
Cuzz <rygus9@naver.com>
2023-11-25 21:35:37 +0900

    feat: 테스트 소켓 연결 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
-export default function App() {
-  return <div>
-    <h1 className="bg-black text-white text-lg">안녕하세요,</h1>
-  </div>
-}
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:5000'); // Flask 서버의 주소
+
+const App = () => {
+  const [message, setMessage] = useState('');
+  const [chat, setChat] = useState<string[]>([]);
+
+  useEffect(() => {
+    socket.on('message', (msg: string) => {
+      setChat(prevChat => [...prevChat, msg]);
+    });
+
+    return () => {
+      socket.off('message');
+    };
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit('message', message);
+    setMessage('');
+  };
+
+  return (
+    <div>
+      <h1>Simple Chat</h1>
+      <input 
+        type="text" 
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyPress={(e) => e.key === 'Enter' ? sendMessage() : null}
+      />
+      <button onClick={sendMessage}>Send</button>
+      <div>
+        {chat.map((msg, index) => (
+          <p key={index}>{msg}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
